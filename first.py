8168edb8e84ea34a43d2bcf275f16bd220138b0e
Shubh Prabhat <shubh.techie@gmail.com>
2025-03-04 12:18:47 -0500

    updated second commit

--- first.py
+++ first.py
@@ -2,3 +2,8 @@ print('hello')
 
 name, age, height = "Rahul", 40, 5.7
 print(name, age, height)
+
+# adding another code to 
+
+sentence = f"name : {name} age: {age} height : {height}"
+print(sentence)
