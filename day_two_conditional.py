b38d4138b0dacab10705b48d36a781661c94fd88
Shubh Prabhat <shubh.techie@gmail.com>
2025-03-18 11:32:55 -0400

    minor changes

--- day_two_conditional.py
+++ day_two_conditional.py
@@ -1,11 +1,19 @@
+a = 8
+b = 3
+print(a)
+print(b)
+print("div :", a/b, "mod: ", a%b, "qutoent", a//b)
+
 
 # print(8%2==0)
 # print(9%2==0)
 # print(10%2==0)
 
-a=8
-if a%2==0:
-    print("8 is an even number")
+# a=8
+# if a%2==0:
+#     print("8 is an even number")
+# else:
+#     print("8 is an odd number")
 
 # a = 8
 # if a % 2 == 0: # If the number is divisible by 2
@@ -16,28 +24,28 @@ if a%2==0:
 #....
 
 
-value = 5
-if value>0:
-    print("The value is positive")
-else:
-    print("The value is negative")
+# value = 5
+# if value>0:
+#     print("The value is positive")
+# else:
+#     print("The value is negative")
 
 
-#---------------------------
+# #---------------------------
 
-x = int(input("enter value x:"))
-y = int(input("enter value of y:"))
+# x = int(input("enter value x:"))
+# y = int(input("enter value of y:"))
 
-if(x>0 and y>0):    
-  print("x and y are positive")
-else:
-    if(x<0 and y >= 0):    
-     print("x or y is second quarant")
-    else:
-        if(x<0 and y < 0):    
-           print("x or y is third quarant")
-        else:
-            if(x>0 and y < 0):    
-               print("x or y is fourth quarant")
-            else:
-                print("x and y are zero")
+# if(x>0 and y>0):    
+#   print("x and y are positive")
+# else:
+#     if(x<0 and y >= 0):    
+#      print("x or y is second quarant")
+#     else:
+#         if(x<0 and y < 0):    
+#            print("x or y is third quarant")
+#         else:
+#             if(x>0 and y < 0):    
+#                print("x or y is fourth quarant")
+#             else:
+#                 print("x and y are zero")
